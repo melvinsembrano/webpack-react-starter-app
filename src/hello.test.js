76bcd3855fe2ee4101661d91c905569f989c5ad3
Melvin Sembrano <melvinsembrano@gmail.com>
2017-06-16 16:27:59 +1000

    update test

--- src/hello.test.js
+++ src/hello.test.js
@@ -2,10 +2,16 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import Hello from './hello'
 
-test('I can see Hello', () => {
+test('I am h1', () => {
   const hello = shallow(
     <Hello name="world" />
   )
   expect(hello.is('h1')).toEqual(true)
+})
+
+test('I can say Hello', () => {
+  const hello = shallow(
+    <Hello name="world" />
+  )
   expect(hello.text()).toEqual('Hello world!')
 })
