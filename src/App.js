7381a6bb3cb665423be1a86cbe84dc5a051cf99f
Melvin Sembrano <melvinsembrano@gmail.com>
2017-10-12 13:02:01 +1100

    covert css to scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style.css'
+import './style.scss'
 import Hello from './hello'
 
 document.addEventListener('DOMContentLoaded', () => {
