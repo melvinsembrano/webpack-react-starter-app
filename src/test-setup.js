29a3e370e774f6be03baa183fdca1df77cfdc009
Melvin Sembrano <melvinsembrano@gmail.com>
2017-12-04 15:53:44 +1100

    upgrade modules

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,4 +1,8 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+const raf = global.requestAnimationFrame = (cb) => {
+  setTimeout(cb, 0)
+}
+
 Enzyme.configure({ adapter: new Adapter() })
